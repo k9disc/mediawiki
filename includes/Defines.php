d79b4fa15f5bc9acc6734b752d452f86c5586453
trainbranchbot <trainbranchbot@releases-jenkins.wikimedia.org>
2021-06-01 02:07:48 +0000

    Branch commit for wmf/1.37.0-wmf.8
    Change-Id: I0b95ac3ee68b16c840e2b6d7a3ba0cc4b49463fb

--- includes/Defines.php
+++ includes/Defines.php
@@ -33,7 +33,7 @@ use Wikimedia\Rdbms\IDatabase;
  *
  * @since 1.35
  */
-define( 'MW_VERSION', '1.37.0-alpha' );
+define( 'MW_VERSION', '1.37.0-wmf.8' );
 
 /** @{
  * Obsolete IDatabase::makeList() constants
