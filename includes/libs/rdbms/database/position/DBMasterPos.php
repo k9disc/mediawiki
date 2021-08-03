2d4ea752ec6f412ba071ef46023c978d55afcd98
Urbanecm <martin.urbanec@wikimedia.cz>
2021-08-03 19:42:33 +0000

    Add (MySQL/DB)PrimaryPos as an alias to (MySQL/DB)MasterPos
    Bug: T287988
    Change-Id: I845a05b9cdd146dca9c1236e2ab81e4529906377

--- includes/libs/rdbms/database/position/DBMasterPos.php
+++ includes/libs/rdbms/database/position/DBMasterPos.php
@@ -38,3 +38,8 @@ interface DBMasterPos extends Serializable {
 	 */
 	public function __toString();
 }
+
+/**
+ * HACK for wmf.16: be able to unserialize cache entries using the new wmf.17 class name (T287988)
+ */
+class_alias( DBMasterPos::class, 'Wikimedia\\Rdbms\\DBPrimaryPos' );
